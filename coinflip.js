60f6c62cba068784db2b36f38f925d73cc62a32f
Kassandra Chase Tramel <kassandra.tramel@knights.ucf.edu>
2016-11-26 23:35:52 +0000

    Project complete.

---
+++ coinflip.js
@@ -0,0 +1,32 @@
+//Function takes an int, flips a coin that many times
+//Write to console xHeads, xTails
+
+
+var prompt = require('prompt');
+prompt.start();
+
+var flipper = function (i) {
+    var heads = 0;
+    var tails = 0;
+    while (i > 0) {
+        var f = Math.random() * 1;
+        if (f < .5) {
+            heads++;
+        } else {
+            tails++;
+        }
+        i--;
+    }
+    console.log("There were " + heads + " heads and " + tails + " tails!");
+};
+
+prompt.get({
+    properties: {
+      flips: {
+        description: ("How many times should the coin be flipped?")
+      }
+    }
+  }, function (err, result) {
+    console.log("The coin will be flipped " + result.flips + " times.");
+    flipper(result.flips);
+  });
